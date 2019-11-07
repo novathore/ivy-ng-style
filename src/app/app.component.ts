d1fe2bf238137b498aab2c74988285787222f2e4
dmitry.vasiliev <dmitry.vasiliev@billing.ru>
2019-11-07 15:02:32 +0300

    ivy reproduce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ReplaySubject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div [ngStyle]="styleObs$ | async">
+        Some content
+    </div>
+
+    <div [ngStyle]="styleVar">
+        Some content
+    </div>
+
+    <button (click)="changeStyle({background: 'cyan'})">Add style</button>
+    <button (click)="changeStyle(null)">Delete style</button>
+  `,
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-ivy-bug';
+  public styleObs$ = new ReplaySubject();
+  public styleVar;
+
+  ngOnInit(): void {
+    this.styleObs$.next({background: 'red'});
+    setTimeout(() => this.styleObs$.next(null), 1000);
+  }
+
+  changeStyle(style) {
+    this.styleVar = style;
+  }
 }
